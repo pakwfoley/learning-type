0c9897daa48d5b10b484d64ba22f23116fe8a5a4
Patrick Foley <patrick.foley@procore.com>
2017-09-27 09:44:21 -0700

    Refactored LearningType.rb

--- spec/learning_type_spec.rb
+++ spec/learning_type_spec.rb
@@ -4,20 +4,55 @@ RSpec.describe LearningType do
   it "has a version number" do
     expect(LearningType::VERSION).not_to be nil
   end
-end
 
-RSpec.describe LearningType::Start do
-  it "Gives instructions to start the game" do
-    expect($stdin).to receive(:puts)
-    $stdin.puts
+  context "#welcome" do
+    it "should welcome the user" do
+      expect(LearningType.welcome).to include("Welcome")
+    end
   end
-end
 
-RSpec.describe LearningType::Game do
-  describe 'get_user_input' do
-    it 'gets  user_input' do
-      expect($stdin).to receive(:gets)
-      $stdin.gets
+  context "#start_game" do
+    it "should start the game" do
+      game = double(:start)
+      expect(game).to receive(:start)
+      LearningType.start_game(game)
     end
   end
 end
+
+# RSpec.describe LearningType::Start do
+#   it "Gives instructions to start the game" do
+#     expect($stdin).to receive(:puts)
+#     $stdin.puts
+#
+#   it " calls instructions when user press enter" do
+#     before do
+#       enter = ""
+#     end
+#   end
+#   end
+#
+#   it 'gets user_input' do
+#     expect($stdin).to receive(:gets)
+#     $stdin.gets
+#   end
+# end
+#
+# RSpec.describe LearningType::Instructions do
+#   it "Gives instructions to play the game" do
+#     expect($stdin).to receive(:puts)
+#     $stdin.puts
+#   end
+#
+#   it 'gets  user_input' do
+#     expect($stdin).to receive(:gets)
+#     $stdin.gets
+#   end
+# end
+#
+# RSpec.describe LearningType::Game do
+#   it 'gets  user_input' do
+#     expect($stdin).to receive(:gets)
+#     $stdin.gets
+#   end
+# end
